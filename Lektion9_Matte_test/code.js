6f5fbc710eee99252787a599765f446397375c61
Nisse Ekström <bojeppe1@gmail.com>
2026-09-17 14:03:59 +0200

    Fix input validation and enhance question display in Matte-TEST

--- Lektion9_Matte_test/code.js
+++ Lektion9_Matte_test/code.js
@@ -5,18 +5,30 @@ let number1 = 0;
 let number2 = 0 ;
 let numbOfTests =0;
 let correctAnswer = 0;
+let yourAnswer;
 
 const numbOfTestsInput = document.querySelector("#numbOfTests");
 const showQuestionsBtn = document.querySelector("#showQuestionBtn");
+
+//Mit fell är check answer - kolla screen grabsen på discord
 const showQuestion = document.querySelector("#showQuestion");
+const showResult = document.querySelector("#checkAnswerBtn");
+const yourAnswerInput = document.querySelector("#yourAnswer");
+
  
 // AADD LISTENER TO BUTTON
 
 showQuestionsBtn.addEventListener(
 "click",
 showTest
+);
 
+//add listener again but too CheckANSWER
 
+checkAnswerBtn
+addEventListener(
+   "click",
+   checkAnswer
 );
 
 //Function to showing tests
@@ -25,7 +37,11 @@ function showTest(){
 
 numbOfTests = numbOfTestsInput.value;
 
-if(numbOfTests === 0 || numbOfTests ==""){
+if(numbOfTests==""){
+   showQuestion.innerHTML = "Ange antal FRÅGOR"
+   return;
+}
+if(numbOfTests === 0){
    showQuestion.innerHTML = "Ange antal FRÅGOR"
     return;
 }
@@ -35,13 +51,29 @@ number1 = Math.round(Math.random() * 10);
 number2 = Math.round(Math.random() * 10);
 correctAnswer = number1 + number2;
 
-showQuestion.innerHTML = "Vad är: " +number1 "+" + number2 + "?";
-showQuestion.innerHTML = number1;
+showQuestion.innerHTML = "Vad är: " + number1 + " + " + number2 + "?";
 
+numbOfTests--;
 }
 // add functions
 
 function calculateFUNC(){
    number1 =0;
    let number3 = 20;
+}
+
+function checkAnswer(){
+
+//här ska jag kontrollera om correctAnswer 
+//är samma som MyAnswer
+yourAnswer = yourAnswerInput.value;
+
+if(correctAnswer ===0){
+   showResult.innerHTML ="Rätt";
+}
+else{
+   showResult.innerHTML ="Fel";
+}
+
+
 }
